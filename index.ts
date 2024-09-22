0570ca9296587af7980942e7a5ffea6870cbb1cf
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-22 17:28:46 +0200

    demo done

--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
-// Welcome to your first TypeScript file.
+// Welcome to your second Typescript demo.
+// TODO: IMPORTANT! To run the code below and what you are about to do, go to your terminal and run "npm run dev"
+
+console.log("WELCOME TO DEMO #2")
 
 // TypeScript is like JavaScript + Types, meaning that you MUST specify types and a description to your code.
 // Look at these examples before you jump into the code
 
+// This is how we write a functions
+/**
+   * Print a name
+   * @param name name to print
+   */
+const printMyName = (name: string): string => {
+  return `My name is ${name}`
+}
 
-  // TODO 1: Create a console log that prints "Hello World using TypeScript"
-  console.log("Hello World using TypeScript");
+// TODO 1: Print your name
+console.log(printMyName("Alejandro"))
+
+// TODO 2: Create a function called addNumbers. The function receives two parameters and both must have type number
+// hint: See https://www.typescriptlang.org/docs/handbook/functions.html
+
+const addNumbers = (a: number, b: number): number => {
+  return a + b
+}
 
 
-  // TODO 2: Now you need to transpile the TypeScript file into a JavaScript file.
-  // TODO 2: In order to do so, go to your terminar and "tsc",
-  // TODO 2: Once you run that command, you should see that a "dist" folder was created. And a new file called "index.js".
-  // NOTE that for every change you make in this file, you need to run "tsc", then "node dist/index.js".
-
-
-
-  // TODO 3: Run "index.js" by typing "node index.js" in your terminal.
-  // TODO 3: Check that you see "Hello World using TypeScript" in the console
-
-
-  // TODO 4: 🎊️ Congrats! You just created your first Hello World using TypeScript.
-
+// TODO 6: call the function addNumbers in a console log. Make sure the functions shows the correct result, f.eks: "2 + 2 is 4."
+console.log("2 + 2 is " + addNumbers(2, 2))
 
 
 
