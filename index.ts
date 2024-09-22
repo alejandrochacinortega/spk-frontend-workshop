291b768a40acabf5d19ebb771e3e8d79a9a538a9
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-22 16:43:22 +0200

    done demo

---
+++ index.ts
@@ -0,0 +1,26 @@
+// Welcome to your first TypeScript file.
+
+// TypeScript is like JavaScript + Types, meaning that you MUST specify types and a description to your code.
+// Look at these examples before you jump into the code
+
+
+  // TODO 1: Create a console log that prints "Hello World using TypeScript"
+  console.log("Hello World using TypeScript");
+
+
+  // TODO 2: Now you need to transpile the TypeScript file into a JavaScript file.
+  // TODO 2: In order to do so, go to your terminar and "tsc",
+  // TODO 2: Once you run that command, you should see that a "dist" folder was created. And a new file called "index.js".
+  // NOTE that for every change you make in this file, you need to run "tsc", then "node dist/index.js".
+
+
+
+  // TODO 3: Run "index.js" by typing "node index.js" in your terminal.
+  // TODO 3: Check that you see "Hello World using TypeScript" in the console
+
+
+  // TODO 4: 🎊️ Congrats! You just created your first Hello World using TypeScript.
+
+
+
+
