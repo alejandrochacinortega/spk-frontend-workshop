814bda6b055098d3001c0e5da1134b0866330868
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-22 18:59:27 +0200

    set error on

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ export default tseslint.config(
   {
     rules: {
         "no-undef": "off",
-        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 
